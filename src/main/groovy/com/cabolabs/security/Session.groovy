4f29f83f329f243f582b49d906e8b11e71b984a5
Pablo Pazos <pablo.pazos@cabolabs.com>
2020-04-27 22:23:42 -0300

    closes #5

--- src/main/groovy/com/cabolabs/security/Session.groovy
+++ src/main/groovy/com/cabolabs/security/Session.groovy
@@ -5,6 +5,7 @@ class Session {
    //String uid = java.util.UUID.randomUUID() as String
    String jsessid
    String userId
+   String userIdFieldName // name of the field in the client app used to identify the user like "username" or "email"
    Date createdOn = new Date()
    boolean authenticated
 
